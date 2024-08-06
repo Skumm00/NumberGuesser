a872c74320cfebb62365be1f4e2947bc2011630d
Skand Bajpai <99634361+Skumm00@users.noreply.github.com>
2024-08-06 19:33:26 +0000

    Found why the helpline function wasnt working, fixed errors, implememnted a naming system for the two ppl and also started the score system

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import pandas 
 
-import math 
 # Initialize variables
 
 score = 0
@@ -9,6 +7,8 @@ chances = 5
 # Starting print statement
 print("Oy! Come on down and welcome to the QUIZ game! You will have 5 chances but you can call for a helpline!")
 
+#get the users name 
+name = input("What's your name?")
 # Get the guess from the first player
 guess = int(input("Enter a number from 1-100: "))
 if guess > 100:
@@ -17,11 +17,34 @@ if guess > 100:
 elif guess <= 0:
     print("Please put a number above zero!")
     guess = int(input("Enter a number from 1-100: "))
-else:
-    print("Please put a proper number")
-    guess = int(input("Enter a number from 1-100: "))  # Runs all the checks
+#inputting the different names
+name2 = input("Player 2, whats your name?")
+print(f"Great! Let's begin! {name}, step back from the computer and let {name2} guess!")
+def keepscore():
+    global score 
+    global score2
+    if score > 5:
+        print(f"Congratulations {name}, you won!")
+    else:
+        print(f"Congratulations {name2}, you won!")
 
-print("Great! Let's begin! Player 1, step back from the computer and let player 2 guess!")
+# Now creating the helpline feature
+helpline_used = False
+def helpline():
+    global helpline_used  # Access helpline
+
+    if helpline_used:
+        print("Sorry, you've already used the helpline once. No more cheater!")
+    elif guess % 2 == 0:
+        print("The Number is even!") #getting more things you can get in helpline
+        helpline_used = True
+    elif guess / 4 == 0:
+        print("The Number is divisible by 4!")
+        helpline_used = True
+    else:
+        math = guess * 2
+        print(f"Oy! Need some help, eh? Well, you're in luck! The number multiplied by 2 equals {math}! Now you'll get it for sure!")
+        helpline_used = True  # Mark helpline as used so cant be modified
 
 # Keep doing until player 2 runs out of chances
 while chances > 0:
@@ -45,17 +68,6 @@ while chances > 0:
         chances -= 1
 
 if chances == 0:
-    print("Sorry mate, you're OUT. Good luck next time!")
+    print(f"Sorry {name2}, you're OUT. Good luck next time!")
+    score += 1
 
-# Now creating the helpline feature
-helpline_used = False
-
-def helpline():
-    global helpline_used  # Acces helpline
-
-    if helpline_used:
-        print("Sorry, you've already used the helpline once. No more cheater!")
-    else:
-        math = guess * 2
-        print(f"Oy! Need some help, eh? Well, you're in luck! The number multiplied by 2 equals {math}! Now you'll get it for sure!")
-        helpline_used = True  # Mark helpline as used so cant be modified
